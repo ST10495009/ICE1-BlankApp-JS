8c34dbf10ec19def7a79d4525612c6e41f92337b
Jahnavi Soni <st10495009@vcconnect.edu.za>
2025-07-31 22:14:06 +0200

    Msg

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count,setCount] = useState<number>(0);
+
+ // const handlePress = () => {console.log("you clicked the button")}; 
+  const handlePress = () => {setCount(count + 1)};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Text>COMPONENTS V1</Text> 
+     <Text>{count}</Text>
+     <Button
+       title="CLICK HERE"
+       onPress={handlePress}
+     />
+    
     </View>
   );
 }
